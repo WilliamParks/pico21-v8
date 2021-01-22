4e8ba9019c667be9c4a07b6dc28a31c71f3d5476
WilliamParks <albntomat0@gmail.com>
2021-01-21 21:37:29 -0500

    Prob 2 base, still needs solution

--- download_horsepower/compile.sh
+++ download_horsepower/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 # Needs to be run from the root of the v8 directory
-gn gen out/kit_engine --args='v8_use_external_startup_data=false is_component_build=false target_cpu="x64" v8_monolithic=true is_debug=true symbol_level=2'
 gn gen out/download_horsepower --args='v8_use_external_startup_data=false is_component_build=false v8_monolithic=true is_debug=false v8_enable_object_print=true symbol_level=2 target_cpu="x64"'
-
-ninja -C ./out/kit_engine d8 
+ninja -C ./out/download_horsepower d8 
