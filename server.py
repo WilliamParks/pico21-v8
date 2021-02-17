c062cdf5acad336398925d1a10eab2346d5043df
m4dst4cks <johnsonjspx@gmail.com>
2021-02-17 14:28:41 +0000

    Updated server.py

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ script_contents = sys.stdin.read(input_size)
 with tempfile.NamedTemporaryFile(buffering=0) as f:
     f.write(script_contents.encode("utf-8"))
     print("File written. Running. Timeout is 20s")
-    res = subprocess.run(["./d8", f.name], timeout=20, capture_output=True)
+    res = subprocess.run(["./d8", f.name], timeout=20, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("Run Complete")
     print(f"Stdout {res.stdout}")
     print(f"Stderr {res.stderr}")
